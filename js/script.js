0fc5185dae474299cb4ae8e1f0311e678bc51e1a
bubblegumi <eliamoftd@gmail.com>
2020-08-05 18:34:04 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -1,105 +1,61 @@
 
+const prev = document.getElementById('btn-prev'),
+      next = document.getElementById('btn-next'),
+      slides = document.querySelectorAll('.slide'),
+      dots = document.querySelectorAll('.dot');
 
-// Texts inputs
+let index = 0;
 
-const totalCost = document.getElementById('total-cost'),
-        anInitialFee = document.getElementById('an-initial-fee'),
-        crediTerm = document.getElementById('credit-term');
-
-// Range inputs
-
-const totalCostRange = document.getElementById('total-cost-range'),
-        anInitialFeeRange = document.getElementById('an-initial-fee-range'),
-        crediTermRange = document.getElementById('credit-term-range');
-
-// Final inputs
-
-const totalAmountOfCredit = document.getElementById('amount-of-credit'),
-        totalMonthlyPayment = document.getElementById('monthly-payment'),
-        totalRecomendedIncome = document.getElementById('recomended-income');
-
-// all rangas
-
-const inputsRange = document.querySelectorAll('.input-range');
-
-// All buttons
-
-const bankBtns =  document.querySelectorAll('.bank');
-
-const assignValue = () => {
-    totalCost.value = totalCostRange.value;
-    anInitialFee.value = anInitialFeeRange.value;
-    crediTerm.value = crediTermRange.value;
-};
-
-assignValue();
-
-const banks = [
-    {
-        name: 'dimasbank',
-        percents: 8.6
-    },
-    {
-        name: 'denisbank',
-        percents: 8.3
-    },
-    {
-        name: 'mashabank',
-        percents: 8.9
-    },
-    {
-        name: 'jekabank',
-        percents: 9.7
+const activeSlide = i => {
+    console.log(i);
+    for(let slide of slides) {
+        slide.classList.remove('active');
     }
-];
-
-let currentPercent = banks[0].percents;
-
-for(let bank of bankBtns) {
-    bank.addEventListener('click', () => {
-        for(let item of bankBtns) {
-            item.classList.remove('active');
-        }
-        bank.classList.add('active');
-        takeActiveBank(bank);
-    });
-}
-
-const takeActiveBank = currentActive => {
-    const dataAttrValue = currentActive.dataset.name;
-    const currentBank = banks.find( bank => bank.name === dataAttrValue);
-    currentPercent = currentBank.percents;
-    calculation(totalCost.value, anInitialFee.value, crediTerm.value);
+    slides[i].classList.add('active');
 };
 
-for(let input of inputsRange){
-    input.addEventListener('input', () => {
-        assignValue();
-        calculation(totalCost.value, anInitialFee.value, crediTerm.value);
-    });
-}
+const activeDot = i => {
+    console.log(i);
+    for(let dot of dots) {
+        dot.classList.remove('active');
+    }
+    dots[i].classList.add('active');
+};
 
-const calculation = (totalCost = 0, anInitialFee = 10000000, crediTerm = 1) => {
-    /*  
-        MP = monthly payment
-        AC = Amount of credit
-        IR = Interest rate
-        Am = amounts of mounth
-        MP = (AC + (((AC / 100) * IR) / 12) * AM) / AM;
-    */
-    let monthlyPayment; //MP
-    let lounAmount = totalCost - anInitialFee; //AC
-    let interestRate = currentPercent;  //IR
-    let numerOfYears = crediTerm; //amounts of years
-    let numberOfMonths = 12 * numerOfYears; //amounts of mounth
+const prepareCurrentSlide = ind => {
+    activeSlide(ind);
+    activeDot(ind);
+};
 
-    monthlyPayment = (lounAmount + (((lounAmount / 100) * interestRate) / 12) * numberOfMonths) / numberOfMonths;
-    const monthlyPaymentArounded = Math.round(monthlyPayment);
-    if(monthlyPaymentArounded < 0) {
-        return false;
+const nextSlide = () => {
+    if(index == slides.length - 1){
+        index = 0;
+        prepareCurrentSlide(index);
     } else {
-        totalAmountOfCredit.innerHTML = `${lounAmount} $`;
-        totalMonthlyPayment.innerHTML = `${monthlyPaymentArounded} $`;
-        totalRecomendedIncome.innerHTML = `${monthlyPaymentArounded + ((monthlyPaymentArounded / 100) * 35)}$`;
+        index++;
+        prepareCurrentSlide(index);
     }
 };
+
+const prevSlide = () => {
+    if(index == 0){
+        index = slides.length - 1;
+        prepareCurrentSlide(index);
+    } else {
+        index--;
+        prepareCurrentSlide(index);
+    }
+};
+
+dots.forEach((item, indexDot) => {
+    item.addEventListener('click', () => {
+        index = indexDot;
+        prepareCurrentSlide(index);
+        clearInterval(interval); // остоновка автоматичесого переключателя  после нажатия на dots
+    });
+});
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
+
+const interval = setInterval(nextSlide, 2500); // автоматическое переключание
